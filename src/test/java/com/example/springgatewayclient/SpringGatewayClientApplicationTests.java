d32a997eb92f5b05d602f89adf90f8868c3f17e4
CaoThang <tcao2315@gmail.com>
2022-10-31 03:16:00 +0700

    finish gateway

---
+++ src/test/java/com/example/springgatewayclient/SpringGatewayClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgatewayclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
